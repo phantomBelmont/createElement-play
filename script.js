bc74b9238e7db19a6ccfbad256c234d2fcc404de
phantomBelmont <297317600+phantomBelmont@users.noreply.github.com>
2026-09-24 18:12:13 +0900

    script.js の更新

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+    //サービスワーカー登録
+    if('serviceWorker' in navigator){
+      window.addEventListener('load',()=>{
+        navigator.serviceWorker.register('./sw.js').then(reg=>console.log('SW登録成功!',reg)
+        )//thenここまで
+        .catch(err=>console.log('SW登録失敗🥲',err)
+        );//catchここまで
+      }//ロードイベントのアロー関数ここまで
+      );//イベリス ここまで
+    }//ifここまで
 
 
 const B = document.getElementById('btn');
